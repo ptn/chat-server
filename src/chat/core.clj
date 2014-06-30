012f05be9827491fc539dcd42d3f0e7015dd3fb8
Pablo Torres <tn.pablo@gmail.com>
2014-06-30 18:52:46 -0500

    Command architecture
    Defined the interface for chat commands and implemented the two simplest
    ones.

--- src/chat/core.clj
+++ src/chat/core.clj
@@ -3,22 +3,9 @@
             [chat.select :as select]
             [chat.protocol :as protocol])
   (:import [java.nio.channels SelectionKey]
-           [java.nio ByteBuffer]
-           [java.nio.charset Charset]))
+           [java.nio ByteBuffer]))
 
 (def buf-size 8096)
-
-(defn accept-client
-  [key]
-  ;; (.channel key) is the server socket which listens for new
-  ;; connections
-  (-> (.channel key)
-      (.accept)
-      (.configureBlocking false)
-      (.register (.selector key) SelectionKey/OP_READ)
-      (.attach {:rbuf (ByteBuffer/allocate buf-size)
-                :wbuf (ByteBuffer/allocate buf-size)}))
-  (println "Accepted connection"))
 
 (defn- close-conn
   [key]
@@ -27,34 +14,66 @@
   (.close (.channel key))
   (println "Closed" (.channel key)))
 
-(defn- decode-buf
-  [buf]
-  (->> buf (.decode (Charset/defaultCharset)) .toString))
-
-(defn- encode-to-buf
-  [s]
-  (.encode (Charset/defaultCharset) s))
-
-;; for now assumes you get a full command in every read
-(defn- read-client
+(defn- accept-client
   [key]
-  (let [rbuf (-> (.attachment key) (:rbuf) (.clear))
-        bytes-read (.read (.channel key) rbuf)]
-    (if (= bytes-read -1)
-      (close-conn key)
-      (let [msg (decode-buf (.flip rbuf))
-            wbuf (-> (.attachment key) (:wbuf) .clear)]
-        (.put wbuf (encode-to-buf msg))
-        (.flip wbuf)
-        (.write (.channel key) wbuf)))))
+  (let [new-client (.accept (.channel key))]
+    (-> new-client
+        (.configureBlocking false)
+        (.register (.selector key) SelectionKey/OP_READ)
+        (.attach {:rbuf (ByteBuffer/allocate buf-size)
+                  :wbuf (ByteBuffer/allocate buf-size)}))
+    (println "Accepted connection from" new-client)
+    new-client))
+
+(defn- run-handler-for
+  "Runs the handler for the given key and returns all the info
+  necessary for next-handlers-and-usernames to update the handlers and
+  usernames maps.
+
+  The new handler plus the new usernames map are taken from the return
+  value of the handler for the given key, and the channel is deduced
+  from the specific key."
+  [k handlers usernames]
+  (cond
+   (= SelectionKey/OP_ACCEPT (.readyOps k))
+   [protocol/require-login usernames (accept-client k)]
+
+   (= SelectionKey/OP_READ (.readyOps k))
+   (let [handler (get handlers (.channel k))
+         [new-handler new-usernames] (handler usernames
+                                              {:channel (.channel k)
+                                               :rbuf (:rbuf (.attachment k))
+                                               :wbuf (:wbuf (.attachment k))})]
+     [new-handler new-usernames (.channel k)])))
+
+(defn- next-handlers-and-usernames
+  "Update the handlers and usernames maps with the handler, username
+  and channel that correspond to the given key."
+  [[handlers usernames] k]
+  (let [[handler usernames' ch] (run-handler-for k handlers usernames)]
+    ;; handler is overloaded, it means both the next handler and
+    ;; success/failure of the current handler
+    (if handler
+      [(assoc handlers ch handler) usernames']
+      (do
+        (close-conn k)
+        [(dissoc handlers (.channel k))
+         (dissoc usernames (.channel k))]))))
 
 (defn chat-server
   [port]
   (let [server (select/server port)]
-    (loop [clients {}]
-      (let [keys (select/select (:selector server))]
-        (doseq [k keys]
-          (cond
-           (= SelectionKey/OP_ACCEPT (.readyOps k)) (accept-client k)
-           (= SelectionKey/OP_READ   (.readyOps k)) (read-client k)))
-        (recur clients)))))
+    ;; handlers maps key channels to closures that handle them
+    ;; usernames maps channels to usernames
+    ;;
+    ;; maybe it'll be a good idea to wrap these in a type, which would
+    ;; let me have a separate set of usernames and the reverse mapping
+    ;; of usernames to sockets while ensuring consistency to help with
+    ;; efficiency
+    (loop [handlers {}
+           usernames {}]
+      (let [keys (select/select (:selector server))
+            [handlers' usernames'] (reduce next-handlers-and-usernames
+                                           [handlers usernames]
+                                           keys)]
+        (recur handlers' usernames')))))
--- src/chat/protocol.clj
+++ src/chat/protocol.clj
@@ -1,27 +1,61 @@
 (ns chat.protocol
-  (:require [clojure.string :as str]))
+  (:require [clojure.string :as str])
+  (:import [java.nio.charset Charset]))
 
-(defn logged-in? [in-out->username in-out]
-  (contains? @in-out->username in-out))
+;; deals with clients, which are maps composed of :channel, :rbuf and :wbuf
+;;
+;; commands perform the corresponding operation, perhaps modifying the
+;; sockets-to-usernames map, and return the next command to run for
+;; that client plus the new usernames map. If the command may be
+;; assoc'd to the handlers map, then it only takes the usernames map
+;; and the client as parameters, if it's something that requires user
+;; input, then it also takes said input.
+;;
+;; also contains utilities for reading from and writing to sockets,
+;; with buffers
 
-(defn cmd-login [in-out->username in-out username]
-  (dosync
-   ;; ensure protects the ref from modification from other transactions, so from
-   ;; this point on, it is safe to read and then write without fearing that the
-   ;; value may have changed in between.
-   ;;
-   ;; how to test this?
-   (let [ensured (ensure in-out->username)]
-     ;; running vals and then some traverses the map twice (or a data structure
-     ;; of that same length)
-     (when-not (some #{username} (vals ensured))
-       (alter in-out->username assoc in-out username)))))
+(declare exec-cmd)
+(declare require-login)
 
-(def cmds {:login cmd-login})
+(defn- encode-to-buf
+  [s]
+  (.encode (Charset/defaultCharset) s))
+
+;; for now assumes you get a full command in every read
+(defn- read-client
+  [client]
+  (.clear (:rbuf client))
+  (let [bytes-read (.read (:channel client) (:rbuf client))]
+    (when-not (= bytes-read -1)
+      (->> (.flip (:rbuf client))
+           (.decode (Charset/defaultCharset))
+           .toString))))
+
+(defn- write-to-client
+  [client msg]
+  (.clear (:wbuf client))
+  (let [encoded (.encode (Charset/defaultCharset) (str msg "\n"))]
+    (.put (:wbuf client) encoded)
+    (.flip (:wbuf client))
+    (.write (:channel client) (:wbuf client))))
+
+(defn cmd-login
+  [usernames client username]
+  (if-not (some #{username} (vals usernames))
+    (do
+      (write-to-client client (str "Welcome " username "!"))
+      [exec-cmd
+       (assoc usernames (:channel client) username)])
+    (do
+      (write-to-client client "Username taken")
+      [require-login usernames])))
 
 (defn cmd-send-msg
-  [in-out->username in-out msg]
-  (println msg))
+  [usernames client msg]
+  (write-to-client client msg)
+  [exec-cmd usernames])
+
+(def cmds {:login cmd-login})
 
 (defn parse-cmd
   [line]
@@ -34,4 +68,22 @@
         [cmd args])
       [cmd-send-msg line])))
 
-(defn exec-cmd [x] x)
+(defn exec-cmd
+  [usernames client]
+  (when-let [msg (read-client client)]
+    (let [[cmd args] (parse-cmd (str/trim msg))]
+      (if (= cmd cmd-login)
+        (do
+          (write-to-client client "Already logged in!")
+          [exec-cmd usernames])
+        (cmd usernames client args)))))
+
+(defn require-login
+  [usernames client]
+  (when-let [msg (read-client client)]
+    (let [[cmd args] (parse-cmd (str/trim msg))]
+      (if (= cmd cmd-login)
+        (cmd-login usernames client args)
+        (do
+          (write-to-client client "Need to login!")
+          [require-login usernames])))))
